57c9943ab7129f8b9242a3d61c95f91cad7f960a
Thibaut Durand <durand.tibo@gmail.com>
2024-11-07 13:20:06 -0800

    Update factory module (#67)

--- src/lightcat/utils/__init__.py
+++ src/lightcat/utils/__init__.py
@@ -1,3 +1,5 @@
+r"""Contain utility functions."""
+
 __all__ = ["setup_object"]
 
 from lightcat.utils.factory import setup_object
--- src/lightcat/utils/factory.py
+++ src/lightcat/utils/factory.py
@@ -1,7 +1,11 @@
+r"""Contain object factory utility functions."""
+
+from __future__ import annotations
+
 __all__ = ["setup_object", "str_target_object"]
 
 import logging
-from typing import TypeVar, Union
+from typing import TypeVar
 
 from objectory import OBJECT_TARGET, factory
 
@@ -10,60 +14,59 @@ logger = logging.getLogger(__name__)
 T = TypeVar("T")
 
 
-def setup_object(obj_or_config: Union[T, dict]) -> T:
-    r"""Sets up an object from its configuration.
+def setup_object(obj_or_config: T | dict) -> T:
+    r"""Set up an object from its configuration.
 
     Args:
-    ----
-        obj_or_config: Specifies the object or its configuration.
+        obj_or_config: The object or its configuration.
 
     Returns:
-    -------
         The instantiated object.
 
     Example usage:
 
-    .. code-block:: pycon
+    ```pycon
 
-       >>> from lightcat.utils import setup_object
-       >>> linear = setup_object(
-       ...     {"_target_": "torch.nn.Linear", "in_features": 4, "out_features": 6}
-       ... )
-       >>> linear
-       Linear(in_features=4, out_features=6, bias=True)
-       >>> setup_object(linear)  # Do nothing because the module is already instantiated
-       Linear(in_features=4, out_features=6, bias=True)
+    >>> from lightcat.utils.factory import setup_object
+    >>> linear = setup_object(
+    ...     {"_target_": "torch.nn.Linear", "in_features": 4, "out_features": 6}
+    ... )
+    >>> linear
+    Linear(in_features=4, out_features=6, bias=True)
+    >>> setup_object(linear)  # Do nothing because the module is already instantiated
+    Linear(in_features=4, out_features=6, bias=True)
+
+    ```
     """
     if isinstance(obj_or_config, dict):
         logger.info(
-            "Initializing an object from its configuration... "
-            f"{str_target_object(obj_or_config)}"
+            f"Initializing {str_target_object(obj_or_config)} object from its configuration... "
         )
-        obj_or_config = factory(**obj_or_config)
+        return factory(**obj_or_config)
     return obj_or_config
 
 
 def str_target_object(config: dict) -> str:
-    r"""Gets a string that indicates the target object in the config.
+    r"""Get a string that indicates the target object in the config.
 
     Args:
-    ----
-        config (dict): Specifies a config compatible with the
-            ``objectory`` library. This dict is expected to have a
-            key ``'_target_'`` to indicate the target object.
+        config: A config using the ``objectory`` library.
+            This dict is expected to have a key ``'_target_'`` to
+            indicate the target object.
 
     Returns:
-    -------
         str: A string with the target object.
 
     Example usage:
 
-    .. code-block:: pycon
+    ```pycon
 
-        >>> from lightcat.utils.factory import str_target_object
-        >>> str_target_object({"_target_": "something.MyClass"})
-        [_target_: something.MyClass]
-        >>> str_target_object({})
-        [_target_: N/A]
+    >>> from lightcat.utils.factory import str_target_object
+    >>> str_target_object({OBJECT_TARGET: "something.MyClass"})
+    something.MyClass
+    >>> str_target_object({})
+    N/A
+
+    ```
     """
-    return f"[{OBJECT_TARGET}: {config.get(OBJECT_TARGET, 'N/A')}]"
+    return config.get(OBJECT_TARGET, "N/A")
