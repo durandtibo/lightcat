b937f9a989551b53224db0e73990a7742bdf606b
Thibaut Durand <durand.tibo@gmail.com>
2024-11-08 09:59:06 -0800

    Add `BaseModelCreator`

---
+++ src/lightcat/model/creator/__init__.py
@@ -0,0 +1,11 @@
+r"""Contain the model creators."""
+
+from __future__ import annotations
+
+__all__ = ["BaseModelCreator", "is_model_creator_config", "setup_model_creator"]
+
+from lightcat.model.creator.base import (
+    BaseModelCreator,
+    is_model_creator_config,
+    setup_model_creator,
+)
