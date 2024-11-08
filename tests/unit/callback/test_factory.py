af2457f7b639a6b5e3a612c20a0ff6c51f2b02c7
Thibaut Durand <durand.tibo@gmail.com>
2024-11-07 19:35:01 -0800

    Add `setup_list_callbacks` (#70)

--- tests/unit/callback/test_factory.py
+++ tests/unit/callback/test_factory.py
@@ -6,9 +6,9 @@ from unittest.mock import patch
 
 import pytest
 import torch
-from lightning.pytorch.callbacks import EarlyStopping
+from lightning.pytorch.callbacks import EarlyStopping, ModelSummary
 
-from lightcat.callback import is_callback_config, setup_callback
+from lightcat.callback import is_callback_config, setup_callback, setup_list_callbacks
 from lightcat.testing import objectory_available
 from lightcat.utils.imports import is_objectory_available
 
@@ -19,6 +19,7 @@ if is_objectory_available():
     from objectory import OBJECT_TARGET
 else:  # pragma: no cover
     OBJECT_TARGET = "_target_"
+
 
 ########################################
 #     Tests for is_callback_config     #
@@ -55,12 +56,6 @@ def test_setup_callback(callback: Callback | dict) -> None:
 
 
 @objectory_available
-def test_setup_callback_object() -> None:
-    callback = EarlyStopping(monitor="loss")
-    assert setup_callback(callback) is callback
-
-
-@objectory_available
 def test_setup_callback_incorrect_type(caplog: pytest.LogCaptureFixture) -> None:
     with caplog.at_level(level=logging.WARNING):
         assert isinstance(setup_callback({OBJECT_TARGET: "torch.nn.Identity"}), torch.nn.Identity)
@@ -73,3 +68,25 @@ def test_setup_callback_object_no_objectory() -> None:
         pytest.raises(RuntimeError, match="'objectory' package is required but not installed."),
     ):
         setup_callback({OBJECT_TARGET: "torch.nn.ReLU"})
+
+
+##########################################
+#     Tests for setup_list_callbacks     #
+##########################################
+
+
+@objectory_available
+def test_setup_list_callbacks() -> None:
+    callbacks = setup_list_callbacks(
+        [
+            {OBJECT_TARGET: "lightning.pytorch.callbacks.EarlyStopping", "monitor": "loss"},
+            {OBJECT_TARGET: "lightning.pytorch.callbacks.ModelSummary"},
+        ]
+    )
+    assert len(callbacks) == 2
+    assert isinstance(callbacks[0], EarlyStopping)
+    assert isinstance(callbacks[1], ModelSummary)
+
+
+def test_setup_list_callbacks_empty() -> None:
+    assert setup_list_callbacks([]) == []
